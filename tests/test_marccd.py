6a3999499a98e290582196dea15384fc8ec0fbc4
Jack Greisman <Jack.Greisman@gmail.com>
2020-02-09 23:05:03 -0500

    Initialize testing framework for MarCCD class

--- tests/test_marccd.py
+++ tests/test_marccd.py
@@ -2,9 +2,19 @@ import marccd
 import unittest
 import pytest
 import numpy as np
+from os.path import join, abspath, dirname
 
 class TestMarCCD(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        cls.testImage = join(abspath(dirname(__file__)),
+                             "data", "e074a_off1_011.mccd")
+        return
+    
     def test_constructor(self):
         self.assertTrue(True)
+        temp = marccd.MarCCD(self.testImage)
+        self.assertTrue(temp.image is not None)
         return
+
