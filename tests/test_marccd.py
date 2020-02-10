d1d128ff49a938eeb82ec998e4b1ee29f5e0e02d
Jack Greisman <Jack.Greisman@gmail.com>
2020-02-09 22:47:33 -0500

    Add infrastructure for unit testing with pytest

---
+++ tests/test_marccd.py
@@ -0,0 +1,10 @@
+import marccd
+import unittest
+import pytest
+import numpy as np
+
+class TestMarCCD(unittest.TestCase):
+
+    def test_constructor(self):
+        self.assertTrue(True)
+        return
