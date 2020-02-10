4ca9193c9581755bc56803dfe37af08a55582c54
Jack Greisman <Jack.Greisman@gmail.com>
2020-02-10 17:37:01 -0500

    Add unit tests for _io.mccd.read()

--- tests/_io/test_mccd.py
+++ tests/_io/test_mccd.py
@@ -1,6 +1,7 @@
 import marccd
 from marccd._io import mccd
 import unittest
+import numpy as np
 from os.path import join, abspath, dirname
 
 class TestIOMCCD(unittest.TestCase):
@@ -11,6 +12,22 @@ class TestIOMCCD(unittest.TestCase):
                              "..", "data", "e074a_off1_011.mccd")
         return
 
+    def test_read(self):
+        """Unit tests for mccd.read()"""
+
+        # File does not exist
+        with self.assertRaises(ValueError):
+            mccd.read("fakefile.mccd")
+
+        # File does exist
+        image, metadata, mccdheader = mccd.read(self.testImage)
+        self.assertIsInstance(image, np.ndarray)
+        self.assertIsInstance(metadata, dict)
+        self.assertFalse("dimensions" in metadata)
+        self.assertEqual(3072, len(mccdheader))
+
+        return
+    
     def test_getTIFFHeader(self):
         """Confirm mccd.getTIFFHeader returns correct header"""
 
